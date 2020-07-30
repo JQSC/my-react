d83ac81e87b51ed7194a60c07579d4b4f6f7ce99
池圣齐 <chi.shengqi@cgtn.com>
2020-07-30 16:38:17 +0800

    2

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,12 +1,7 @@
 import { createElement } from './ReactElement';
-import ReactCurrentDispatcher from './ReactCurrentDispatcher';
 
 const React = {
-    createElement,
-    // 用于保存内部使用的一些变量，方便在模块间引用
-    internals: {
-        ReactCurrentDispatcher
-    },
+    createElement
 }
 
 export default React;
